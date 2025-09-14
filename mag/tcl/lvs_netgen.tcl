016e2d7b0b94f9396ca4fd78b7c618309492b3ba
Jeremiasz Hauck <13hihi31@gmail.com>
2025-09-14 20:26:02 +0000

    Use analog input to drive AND gate power

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -8,5 +8,6 @@ readnet verilog ../src/project.v $source
 readnet spice variable_delay_dummy.spice $source
 readnet spice variable_delay_short.spice $source
 readnet spice input_stage.spice $source
+readnet spice input_stage_andpwr.spice $source
 readnet spice tdc_vernier_buffers.spice $source
 lvs "$layout $project" "$source $project" $::env(PDK_ROOT)/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
